6d4073b62ae9f31d86670af7609a38ffd0115e8c
DeividRogerMR <deividroger.1995@gmail.com>
2023-06-10 15:23:08 -0300

    consolelog no indexjs

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,6 +16,7 @@ function RoutesApp() {
         withCredentials: true,
       });
       setUser(data.user);
+      console.log(("user:", user));
     } catch (error) {
       console.log(error);
     }
